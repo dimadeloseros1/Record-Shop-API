a485e3a93cd8448ec24586255b4f790c5b162ec4
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-05 12:31:30 +0100

    Feat: Implement base structure of Controller test

--- src/test/java/com/northcoders/RecordShopApi/Controller/AlbumControllerTests.java
+++ src/test/java/com/northcoders/RecordShopApi/Controller/AlbumControllerTests.java
@@ -1,4 +1,24 @@
 package com.northcoders.RecordShopApi.Controller;
 
+import com.northcoders.RecordShopApi.Service.AlbumServiceImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 public class AlbumControllerTests {
+
+
+    @Mock
+    private AlbumServiceImpl albumServiceImpl;
+
+    @InjectMocks
+    private AlbumController albumController;
+
+    @Autowired
+    private MockMvc mockMvcController;
 }
