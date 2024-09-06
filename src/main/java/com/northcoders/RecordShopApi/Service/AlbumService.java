be7722034fdb09c63bedfb7b14821d61a12d2314
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-06 08:34:54 +0100

    Feat: Implement Interface & updated properties file

--- src/main/java/com/northcoders/RecordShopApi/Service/AlbumService.java
+++ src/main/java/com/northcoders/RecordShopApi/Service/AlbumService.java
@@ -3,7 +3,9 @@ package com.northcoders.RecordShopApi.Service;
 import com.northcoders.RecordShopApi.Model.Album;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AlbumService {
     List<Album> getAllAlbums();
+    Optional<Album> getAlbumById(Long id);
 }
