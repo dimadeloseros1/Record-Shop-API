6afa3d5c1c4e4b69f060aff2a675bd123b30acd7
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-06 08:07:02 +0100

    Test: Implement GET album by id test controller

--- src/test/java/com/northcoders/RecordShopApi/Controller/AlbumControllerTests.java
+++ src/test/java/com/northcoders/RecordShopApi/Controller/AlbumControllerTests.java
@@ -20,6 +20,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
@@ -29,7 +30,7 @@ public class AlbumControllerTests {
 
 
     @Mock
-    private AlbumServiceImpl albumServiceImpl;
+    private AlbumServiceImpl mockAlbumServiceImpl;
 
     @InjectMocks
     private AlbumController albumController;
@@ -47,14 +48,14 @@ public class AlbumControllerTests {
 
     @Test
     @DisplayName("GET all albums & returns all albums")
-    void getAllAlbums() throws Exception {
+    void getAllAlbumsTest() throws Exception {
         List<Album> albums = new ArrayList<>(List.of(
                 new Album(1L, 1, "Ferry Corsten", 1997, Genre.TRANCE, "Rank 1"),
                 new Album(2L, 1, "Armin Van Buuren", 1999, Genre.TRANCE, "Trance Classics"),
-                new Album(3L, 1, "Tiesto", 1997, Genre.TRANCE, "Gold Age Of Trance"),
+                new Album(3L, 1, "Tiesto", 1997, Genre.TRANCE, "Golden Age Of Trance")
         ));
 
-        when(albumServiceImpl.getAllAlbums()).thenReturn(albums);
+        when(mockAlbumServiceImpl.getAllAlbums()).thenReturn(albums);
 
         this.mockMvcController.perform(
                 MockMvcRequestBuilders.get("/api/v1/albums"))
@@ -66,6 +67,28 @@ public class AlbumControllerTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(3L))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[2].artist").value("Tiesto"));
     }
+
+    @Test
+    @DisplayName("GET album by id")
+    public void getAlbumByIdTest() throws  Exception {
+        List<Album> albums = new ArrayList<>(List.of(
+                new Album(1L, 1, "Ferry Corsten", 1997, Genre.TRANCE, "Rank 1"),
+                new Album(2L, 1, "Armin Van Buuren", 1999, Genre.TRANCE, "Trance Classics"),
+                new Album(3L, 1, "Tiesto", 1997, Genre.TRANCE, "Gold Age Of Trance"),
+                new Album(4L, 1, "Tiesto", 1997, Genre.TRANCE, "Gold Age Of Trance")
+        ));
+
+//        Album album = new Album(1L, 1, "Ferry Corsten", 1997, Genre.TRANCE, "Rank 1")
+
+        when(mockAlbumServiceImpl.getAlbumById(1L)).thenReturn(Optional.ofNullable(albums.getFirst()));
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/albums/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath(".id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath(".albumName").value("Rank 1"));
+    }
+
 }
 
 
