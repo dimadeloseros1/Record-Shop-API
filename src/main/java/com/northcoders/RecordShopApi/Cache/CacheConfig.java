bee6cdecb704aee7bf7c565784ec08200586f57f
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-15 15:25:46 +0100

    Feat: Implement caching via Caffeine Cache for GET endpoint

---
+++ src/main/java/com/northcoders/RecordShopApi/Cache/CacheConfig.java
@@ -0,0 +1,35 @@
+package com.northcoders.RecordShopApi.Cache;
+
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+
+public class CacheImpl {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .initialCapacity(10)
+                .maximumSize(500)
+                .expireAfterWrite(300, TimeUnit.SECONDS);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("AlbumsCache");
+        caffeineCacheManager.setCaffeine(caffeineConfig());
+        return caffeineCacheManager;
+    }
+}
