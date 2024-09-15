0662ba49956ed1f2ea303decb779a8721fc95360
dimadeloseros1 <dimadeloseros1@gmail.com>
2024-09-15 15:28:44 +0100

    Feat: Enable caching in the root of the application

--- src/main/java/com/northcoders/RecordShopApi/RecordShopApiApplication.java
+++ src/main/java/com/northcoders/RecordShopApi/RecordShopApiApplication.java
@@ -7,8 +7,11 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cache.annotation.EnableCaching;
+
 
 @SpringBootApplication
+@EnableCaching
 public class RecordShopApiApplication {
 
 	public static void main(String[] args) {
